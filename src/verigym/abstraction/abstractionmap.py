2adc568183531ba8c1cc7fa8cf14d8b4e43c7f9e
Jule Schmidt <jule.schmidt@rub.de>
2025-12-16 19:12:55 +0100

    bugfix and add documentation

--- src/verigym/abstraction/abstractionmap.py
+++ src/verigym/abstraction/abstractionmap.py
@@ -3,6 +3,10 @@ from verigym.environments.explicitenv import ExplicitEnv
 
 
 class AbstractionMap:
+    """
+    Provides a mapping between abstract and original spaces.
+    """
+
     def __init__(self,
                  original_env: VeriGymEnv,
                  abstract_env: ExplicitEnv,
@@ -14,14 +18,65 @@ class AbstractionMap:
 
         self.abstraction_map = abstraction_map
 
-    def abstract_to_original_state(orig_state):
+    def abstract_to_original_state(abs_state):
+        """
+        Maps an abstract state to a set/range of original states.
+
+        Parameters
+        ----------
+        abs_state : object
+            A state in self.abstract_env
+        Returns
+        ----------
+        orig_states : object
+            A set/range of states in self.original_env
+        """
         ... # TODO
 
-    def original_to_abstract_state(abs_state):
+    def original_to_abstract_state(orig_state):
+        """
+        Maps an original state to an abstract state.
+        
+        Parameters
+        ----------
+        orig_state : object
+            A state in self.original_env
+        
+        Returns
+        -------
+        abs_state : object
+            A state in self.abstract_env
+        """
         ... # TODO
 
-    def original_to_abstract_action(orig_state):
+    def original_to_abstract_action(orig_action):
+        """
+        Maps an action in self.original_env to an action in self.abstract_env
+        
+        Parameters
+        ----------
+        orig_action : object
+            An action in the original environment.
+
+        Returns
+        -------
+        abs_action : object
+            A set/range of actions in the abstract environment.
+        """
         ... # TODO
 
     def abstract_to_original_action(abs_action):
+        """
+        Maps an action in self.abstract_env to a set/range of actions in self.original_env
+
+        Parameters
+        ----------
+        abs_action : object
+            An action in the abstract environment
+
+        Returns
+        -------
+        orig_actions : object
+            A set/range of actions in the original environment
+        """
         ... # TODO
--- src/verigym/policy/policy.py
+++ src/verigym/policy/policy.py
@@ -1,6 +1,11 @@
 from verigym.abstraction.abstractionmap import AbstractionMap
 
 class PolicyClass:
+    """
+    An abstract class providing an interface to any kind of model policy.
+    Given an observation in the VeriGym environment,
+    return an action for the VeriGym environment based on the abstract model's policy.
+    """
 
     def __init__(self, 
                  policy,
@@ -12,6 +17,16 @@ class PolicyClass:
     def _action_from_policy(self, obs):
         """
         Get an action from the model's policy.
+
+        Parameters
+        ----------
+        obs : object
+            an observation in the abstract model's space
+        
+        Returns
+        -------
+        action : object
+            an action in the abstract model's space according to the policy
         """
         # This should be implemented in specific child classes
         raise NotImplementedError
@@ -21,9 +36,20 @@ class PolicyClass:
         Gets an observation from `env.observation_space`. 
         Queries the model's policy.
         Map the action from the model's policy to `env.action_space`
+        
+        Parameters
+        ----------
+        obs : object
+            An observation in the environment's observation space
+        
+        Returns
+        -------
+        action : object
+            An action in the environment's action space
+
         """
-        o = self.abstraction_map.abstract_to_original_state(obs) #self._obs_to_model(obs)
+        o = self.abstraction_map.original_to_abstract_state(obs) #self._obs_to_model(obs)
         a = self._action_from_policy(o)
-        action = self.abstract_to_original_action(a)
+        action = self.abstraction_map.abstract_to_original_action(a)
         return action
         
