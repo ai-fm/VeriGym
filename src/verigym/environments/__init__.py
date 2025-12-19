d746f720ce1443b921e1aab0b69d37e679d5447b
Joshua Wendland <80349780+joshuawe@users.noreply.github.com>
2025-12-19 13:12:05 +0100

    define __all__

--- src/verigym/environments/__init__.py
+++ src/verigym/environments/__init__.py
@@ -1,3 +1,9 @@
 from .explicitenv import ExplicitEnv
 from .verigymenv import VeriGymEnv
 from .simulatorenv import SimulatorEnv
+
+__all__ = [
+    "ExplicitEnv",
+    "VeriGymEnv",
+    "SimulatorEnv",
+]
