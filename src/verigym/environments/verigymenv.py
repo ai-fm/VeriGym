2f5fabc781938c328cb3d9d11e1ff190ced954d0
Joshua We <80349780+joshuawe@users.noreply.github.com>
2026-02-13 16:33:28 +0100

    Add `VeriGymEnv.simulate()` function (#46)
    * add VeriGymEnv.simulate function
    
    * fix tests and bugs, when VeriGymEnv.simulate is used now
    
    * 🐶
    
    * fix tests
    
    * clean up comments

--- src/verigym/environments/verigymenv.py
+++ src/verigym/environments/verigymenv.py
@@ -1,4 +1,9 @@
+import logging
+
 import gymnasium as gym
+from numpy.typing import NDArray
+
+logger = logging.getLogger(__name__)
 
 
 class VeriGymEnv(gym.Env):
@@ -7,4 +12,37 @@ class VeriGymEnv(gym.Env):
     Parent to all types of VeriGym environments.
     """
 
-    pass
+    def simulate(
+        self, policy, n_steps: int = 1
+    ) -> list[list[NDArray, NDArray, NDArray, NDArray]]:
+        """
+        Simulate the environment for `n_steps` using the provided `policy`.
+
+        Args:
+            policy: A function that takes an observation and returns an action.
+            n_steps (int): Number of steps to simulate.
+
+        Returns:
+            A list of trajectories containing a list of tuples (state, action, reward, next_state) for each step.
+        """
+        logger.warning(
+            "The VeriGymEnv.simulate() method currently only simlates a random policy."
+        )
+        dataset, trajectory = [], []
+        state, info = self.reset()
+        for _ in range(n_steps):
+            action = self.action_space.sample()  # TODO Replace with policy(obs)
+            next_state, reward, done, truncated, info = self.step(action)
+            trajectory.append((state, action, reward, next_state))
+            state = next_state
+            assert state is not None, "State should not be None after step."
+            if done or truncated:
+                dataset.append(trajectory)
+                state, info = self.reset()
+                trajectory = []
+
+        if len(trajectory) > 0:
+            # Append remaining trajectories
+            dataset.append(trajectory)
+
+        return dataset
