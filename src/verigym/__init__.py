35d1364525f35d183776e2be9def614f879aa8a1
joshuawe <80349780+joshuawe@users.noreply.github.com>
2026-02-16 17:05:19 +0100

    update init

--- src/verigym/__init__.py
+++ src/verigym/__init__.py
@@ -1,36 +1,44 @@
-# import gymnasium as gym
+import gymnasium as gym
 
 # Environments
-from .environments.verigymenv import VeriGymEnv
-from .environments.explicitenv import ExplicitEnv
-from .environments.frameworkexplicitenv import FrameworkExplicitEnv
-from .environments.generativeenv import GenerativeEnv
-from .environments.reward_func import RewardFunction
-from .environments.transition_func import TransitionFunction
-from .environments import exporter
+from .environments import exporter as exporter
+from .environments.explicitenv import ExplicitEnv as ExplicitEnv
+from .environments.frameworkexplicitenv import FrameworkExplicitEnv as FrameworkExplicitEnv
+from .environments.generativeenv import GenerativeEnv as GenerativeEnv
+from .environments.reward_func import RewardFunction as RewardFunction
+from .environments.transition_func import TransitionFunction as TransitionFunction
+from .environments.verigymenv import VeriGymEnv as VeriGymEnv
 
 # Abstraction
-from .abstraction.learn_abstraction import create_abstraction
+from .abstraction.learn_abstraction import create_abstraction as create_abstraction
 
 # Frameworks
-from .frameworks import juliapomdp
-from .frameworks import mujoco
-from .frameworks import stormpy
+from .frameworks import juliapomdp as juliapomdp
+from .frameworks import mujoco as mujoco
+from .frameworks import stormpy as stormpy
 
 # Policies
-from .policy.policy import PolicyClass
-
-
-# gym.register(
-#     id="ExplicitEnv-v0",
-#     entry_point=ExplicitEnv,
-# )
-
-# gym.register(id="FrameworkExplicitEnv-v0", entry_point=FrameworkExplicitEnv)
+from .policy.policy import PolicyClass as PolicyClass
 
 # __all__ = [
-#     ExplicitEnv,
-#     FrameworkExplicitEnv,
-#     GenerativeEnv,
-    
+#     "VeriGymEnv",
+#     "ExplicitEnv",
+#     "FrameworkExplicitEnv",
+#     "GenerativeEnv",
+#     "RewardFunction",
+#     "TransitionFunction",
+#     "exporter",
+#     "create_abstraction",
+#     "juliapomdp",
+#     "mujoco",
+#     "stormpy",
+#     "PolicyClass",
 # ]
+
+
+gym.register(
+    id="ExplicitEnv-v0",
+    entry_point=ExplicitEnv,
+)
+
+gym.register(id="FrameworkExplicitEnv-v0", entry_point=FrameworkExplicitEnv)
