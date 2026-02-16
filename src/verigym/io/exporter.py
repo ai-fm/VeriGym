16e2e9406e8e50612bf9e77c22b4e5391e0414f2
Maris Galesloot <maris.galesloot@ru.nl>
2026-02-16 16:59:13 +0100

    Move things around

--- src/verigym/io/exporter.py
+++ src/verigym/io/exporter.py
@@ -125,4 +125,65 @@ def base_explicit_env_to_umb(env : BaseExplicitEnv) -> umbi.ats.ExplicitAts: # T
     ats.choice_to_branch = []
     ats.branch_to_target = []
     ats.branch_probabilities = []
-    
+
+
+from verigym.environments.base_explicitenv import BaseExplicitEnv
+from verigym.frameworks.stormpy.stormpy_utils import build_stormpy_mdp
+
+import os
+import stormpy
+
+"""
+Methods to export to different formal model formats.
+"""
+
+
+def export_to_stormpy_mdp(env: BaseExplicitEnv) -> stormpy.storage.SparseMdp:
+    """Exports an `ExplicitEnv` to a `stormpy.storage.SparseMdp`.
+
+    Parameters
+    ----------
+    env : ExplicitEnv
+        The explicit environment.
+
+    Returns
+    -------
+    stormpy_mdp : stormpy.storage.SparseMdp
+        The mdp.
+    """
+    assert issubclass(type(env), BaseExplicitEnv)
+    stormpy_mdp = build_stormpy_mdp(env)
+    return stormpy_mdp
+
+
+def export_to_julia(env: BaseExplicitEnv): ...  # TODO @Merlijn
+
+
+def export_to_drn(env: BaseExplicitEnv, out_file: str | None = None) -> None:
+    """
+    Exports an explicit env to an explicit model and writes directly to a .drn file.
+
+    Parameters
+    ----------
+    env : BaseExplicitEnv
+        The explicit environment.
+    out_file : str
+        Path to the output file. By default, writes to cwd/mdp.drn.
+    """
+    if not out_file:
+        out_file = os.path.join(os.getcwd(), "mdp.drn")
+
+    stormpy_mdp = build_stormpy_mdp(env)
+    stormpy.export_to_drn(model=stormpy_mdp, file=out_file)
+
+def export_to_prism(env: BaseExplicitEnv) -> str:
+    """
+    Exports an explicit env to PRISM-readable files
+    .tra for transitions
+    .srew/.trew for rewards
+    .lab for labels
+    """
+    ...  # TODO @Jule/Maris?
+
+def export_to_umb(env: BaseExplicitEnv) -> None:
+    base_explicit_env_to_umb(env)
