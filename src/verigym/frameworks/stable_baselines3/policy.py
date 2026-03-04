ae39f0e09251a646849e23fbd9e46c9553167625
joshuawe <80349780+joshuawe@users.noreply.github.com>
2026-03-04 11:04:29 +0100

    make identity map default in `PolicyClass.__init__`

--- src/verigym/frameworks/stable_baselines3/policy.py
+++ src/verigym/frameworks/stable_baselines3/policy.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from stable_baselines3.common import type_aliases
 
 from verigym.policy.policy import PolicyClass
@@ -9,26 +10,28 @@ __all__ = ["SB3Policy"]
 
 
 class SB3Policy(PolicyClass):
-    def __init__(self, policy: SB3_original_Policy, abstraction_mapper=None):
+    def __init__(
+        self,
+        policy: SB3_original_Policy,
+        abstraction_mapper: Optional[AbstractionMapper] = None,
+    ):
         """
-        Initializes a `verigym` compatible from a policy from the stable baselines 3 (`SB3`) framework.
+        Initializes a `verigym` compatible policy from a policy by the stable baselines 3 (SB3) framework.
 
-        Implements an identity `AbstractionMapper`, meaning there is no abstraction to map from/to.
+        Defaults to an identity `AbstractionMapper`, meaning the policy's actions remain unchanged (as there is no abstraction to map from/to).
 
         Parameters
         ----------
-        policy : sb3_Policy
-            The `SB3` policy. This can be any object that implements a `predict` method such as `SB3`'s `BaseAlgorithm` or `BasePolicy` and child classes.
+        policy : SB3_original_Policy
+            The `SB3` policy. This can be any object that implements a `predict` method such as the SB3 framework's `BaseAlgorithm` or `BasePolicy` and child classes.
+        abstraction_mapper: AbstractionMapper, optional
+            The abstraction mapper may map the action to another action space (e.g. from abstracted to original environment). Defaults to None, meaning the action remains unchanged.
 
         Returns
         -------
         SB3Policy
-            `verigym` compatible policy.
+            A `verigym` compatible policy.
         """
-        # default to identity abstraction mapper
-        if abstraction_mapper is None:
-            abstraction_mapper = AbstractionMapper()
-
         return super().__init__(policy, abstraction_mapper)
 
     def _action_from_policy(self, obs):
