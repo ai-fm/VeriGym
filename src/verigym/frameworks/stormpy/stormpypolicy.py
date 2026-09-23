668d10cbd69483080e2703f385b7a71078ff9521
joshuawe <80349780+joshuawe@users.noreply.github.com>
2026-09-23 17:10:40 +0200

    `StormpyPolicy` now uses abstraction mapper.
    Previously the abstraction mapper was not used, a mistake. Now it uses `original_to_enum` to pass to the stormpy policy.

--- src/verigym/frameworks/stormpy/stormpypolicy.py
+++ src/verigym/frameworks/stormpy/stormpypolicy.py
@@ -7,7 +7,9 @@ class StormpyPolicy(PolicyClass):
         super().__init__(policy=policy, abstraction_mapper=abstraction_mapper)
 
     def _action_from_policy(self, obs):
-        choice = self.policy.get_choice(obs)  # distribution over actions
+        # use abstractionmapper to obtain the enumerated abstract state, which is what the storm policy requires
+        enum = self.abstraction_mapper._state_abstraction_map.original_to_enum(obs)
+        choice = self.policy.get_choice(enum)  # distribution over actions
         action_index = choice.get_deterministic_choice()
         # action = state.actions[action_index]
         return action_index
